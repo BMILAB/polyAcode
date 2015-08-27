fcfdd0b8fe34cc3fc393aee44abeaa1fec5b888e
Yi Li <yili@counsyl.com>
2015-08-27 13:22:07 -0700

    os.system("taskset -p 0xff %d" % os.getpid()) added

--- adaboost.py
+++ adaboost.py
@@ -6,6 +6,8 @@ import numpy as np
 from sklearn import ensemble, tree, metrics, cross_validation
 from matplotlib import pyplot as plt
 
+os.system("taskset -p 0xff %d" % os.getpid())
+
 MAX_DEPTH = 4
 MIN_LEAF = 1
 D = 658
