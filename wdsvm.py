3f8d8cbcedaaf62b12b44bb5fa4eaa190c001a47
Yi Li <yil8@uci.edu>
2015-11-22 11:58:13 -0800

    wdsvm.py: fixed

--- wdsvm.py
+++ wdsvm.py
@@ -5,13 +5,12 @@ import sys
 import numpy as np
 from sklearn import metrics
 from modshogun import BinaryLabels, StringCharFeatures, DNA, WeightedDegreePositionStringKernel, SVMLight
-from matplotlib import pyplot as plt
 
 C_LST = [0.01,0.1,0.5,1.0]
 DEGREE_LST = [5, 10, 15, 20]
 TOL = 0.00001
 D = 658
-K = 10
+# K = 10
 SEQ_LEN = 201
 
 
@@ -31,7 +30,7 @@ def parse(infile):
     return (X, Y)
 
 
-def cross_validation(X, Y, d, c):
+def cross_validation(X, Y, d, c, K):
     N = len(Y)
     n = N/K
     
@@ -83,13 +82,12 @@ def svm_process(args_tuple):
 
 
 def main():
-    base_name = sys.argv[1]
+    intrain = sys.argv[1]
+    intest = sys.argv[2]
+    K = int(sys.argv[3])
     
-    data_train = './data_seq_train.txt'
-    data_test = './data_seq_test.txt'
-    
-    X_train, Y_train = parse(data_train)
-    X_test, Y_test = parse(data_test)
+    X_train, Y_train = parse(intrain)
+    X_test, Y_test = parse(intest)
     
     D_best = 0
     C_best = 0
@@ -97,7 +95,7 @@ def main():
     
     for d in DEGREE_LST:        
         for c in C_LST:
-            accuracy_cv = cross_validation(X_train, Y_train, d, c)
+            accuracy_cv = cross_validation(X_train, Y_train, d, c, K)
             
             if accuracy_cv > accuracy_cv_best:
                 D_best = d
@@ -106,7 +104,8 @@ def main():
         
             print 'degree = %s\tC = %s\tcross_validation_accuracy = %.1f%%' % (d, c, accuracy_cv*100)
             sys.stdout.flush()
-        
+
+    print '****************************************************************************************'
     print 'degree_best = %s\tC_best = %s\tcross_validation_accuracy_best = %.1f%%' % (D_best, C_best, accuracy_cv_best*100)
     
     X_train = StringCharFeatures(X_train, DNA)
@@ -120,22 +119,8 @@ def main():
     auc = metrics.roc_auc_score(Y_test, Y_test_proba)
     fpr, tpr, thresholds = metrics.roc_curve(Y_test, Y_test_proba)
      
-    np.save(base_name + '_fpr.npy', fpr)
-    np.save(base_name + '_tpr.npy', tpr)
-    outfile = open(base_name + '_results.txt', 'w')
-    outfile.write('accuracy\t' + str(accuracy) + '\n')
-    outfile.write('AUC\t' + str(auc) + '\n')
-    outfile.close()
-    
-#     plt.figure(figsize=(10, 8))
-#     plt.plot(fpr, tpr, 'b.')
-#     plt.title('WD SVM; accuracy = %.1f%%; AUC = %.3f' % (accuracy*100, auc))
-#     plt.xlabel('False Positive Rate')
-#     plt.ylabel('True Positive Rate')
-#     plt.xlim((0, 1))
-#     plt.ylim((0, 1))
-#     plt.show()
-
+    print '****************************************************************************************'
+    print 'accuracy : %.1f%%\tAUC : %.3f' % (accuracy*100, auc)
     
     
 if __name__ == '__main__':
